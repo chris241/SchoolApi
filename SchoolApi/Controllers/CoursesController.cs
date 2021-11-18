b123fbd3e88ab2fbd83d1ea8abf571a456762459
chris241 <chrisleon325@gmail.com>
2021-11-18 15:25:33 +0300

    ajout du verbs delete

--- SchoolApi/Controllers/CoursesController.cs
+++ SchoolApi/Controllers/CoursesController.cs
@@ -13,7 +13,7 @@ namespace SchoolApi.Controllers
     public class CoursesController : ControllerBase
     {
         private readonly SchoolApiContext _context;
-        public CoursesController( SchoolApiContext context)
+        public CoursesController(SchoolApiContext context)
         {
             _context = context;
         }
@@ -22,6 +22,35 @@ namespace SchoolApi.Controllers
         {
             var courses = _context.Courses.ToList();
             return courses;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Course> GetCourseById(int id)
+        {
+            var course = _context.Courses.Where(x => x.Id == id).FirstOrDefault();
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return course;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Course>> CreateCourse(Course cours)
+        {
+            var course = _context.Courses.Add(cours);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCourseById), new { id = cours.Id }, cours);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if( course == null)
+            {
+                return NotFound();
+            }
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
         /*
         [HttpGet]
