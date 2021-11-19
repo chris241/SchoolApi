1001e87ce61474879df66729078f8c71564606a8
chris241 <chrisleon325@gmail.com>
2021-11-19 18:35:10 +0300

    change model

---
+++ SchoolApi/Controllers/TeacherController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly SchoolApiContext _context;
+        public TeacherController(SchoolApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Teacher>> GetListTeacher()
+        {
+            var teachers = _context.Teachers.ToList();
+            return teachers;
+        }
+
+    }
+}
